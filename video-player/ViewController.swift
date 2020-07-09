53c4af2db8ba72ceef11f1d689c266407cbdeb0d
Dmytro Golub <Dmytro.Golub@gmail.com>
2020-07-09 09:39:39 +0100

    Initial Commit

---
+++ video-player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video-player
+//
+//  Created by magician on 09/07/2020.
+//  Copyright © 2020 dmytro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
